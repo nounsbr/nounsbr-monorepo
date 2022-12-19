5e1dff8bc3459e2e6c692b3633882165ba98e21d
defnullx <defnullx@outlook.com>
2022-12-19 19:48:11 -0300

    Add Traits Tips

--- packages/nounsbr-webapp/src/components/NounBR/index.tsx
+++ packages/nounsbr-webapp/src/components/NounBR/index.tsx
@@ -2,6 +2,7 @@ import classes from './NounBR.module.css';
 import React from 'react';
 import loadingNounBR from '../../assets/loading-skull-nounbr.gif';
 import Image from 'react-bootstrap/Image';
+import NounBRTraitsOverlay from '../NounBRTraitsOverlay';
 
 export const LoadingNounBR = () => {
   return (
@@ -16,16 +17,18 @@ const NounBR: React.FC<{
   alt: string;
   className?: string;
   wrapperClassName?: string;
+  parts?: { filename: string }[];
 }> = props => {
-  const { imgPath, alt, className, wrapperClassName } = props;
+  const { imgPath, alt, className, wrapperClassName, parts } = props;
   return (
-    <div className={`${classes.imgWrapper} ${wrapperClassName}`}>
+    <div className={`${classes.imgWrapper} ${wrapperClassName}`} data-tip data-for="nounbr-traits">
       <Image
         className={`${classes.img} ${className}`}
         src={imgPath ? imgPath : loadingNounBR}
         alt={alt}
         fluid
       />
+      {Boolean(parts?.length) && <NounBRTraitsOverlay parts={parts!} />}
     </div>
   );
 };
---
+++ packages/nounsbr-webapp/src/components/NounBRTraitsOverlay/index.tsx
@@ -0,0 +1,36 @@
+import classes from './NounBRTraitsOverlay.module.css';
+import React from 'react';
+import ReactTooltip from 'react-tooltip';
+
+const NounBRTraitsOverlay: React.FC<{
+  parts: { filename: string }[];
+}> = props => {
+  const { parts } = props;
+  const getNounBRTrait = (part: { filename: string }) => {
+    const splitData: string[] = part.filename.split('-');
+    return {trait: splitData[0], value: splitData.slice(1).join(' ')}
+  }
+
+  return (
+    <ReactTooltip
+      id="nounbr-traits"
+      place="top"
+      effect="float"
+      backgroundColor="white"
+      textColor="black"
+    >
+      <ul className={classes.traitList}>
+        {parts.map((part) => {
+          const { trait, value } = getNounBRTrait(part);
+          return (
+            <li key={trait}>
+              {trait}: {value}
+            </li>
+          )
+        })}
+      </ul>
+    </ReactTooltip>
+  );
+};
+
+export default NounBRTraitsOverlay;
